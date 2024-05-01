c7bfeb9156c350e897f70d9c6b1c64c8282ad3cc
Alonso Robles <alonsofernando1907@outlook.es>
2024-04-30 21:32:44 -0500

    Implementación de las Pantallas de Inicio del Estudiante y Lista de Profesores

---
+++ app/src/main/java/com/example/studymentor/model/Picture.kt
@@ -0,0 +1,7 @@
+package com.example.studymentor.model
+
+class Picture (
+    val large: String,
+    val medium: String,
+    val thumbnail: String
+)
---
+++ app/src/main/java/com/example/studymentor/model/tutorResponse.kt
@@ -0,0 +1,5 @@
+package com.example.studymentor.model
+
+data class TutorResponse (
+    val results: List<UserTutor>
+)
