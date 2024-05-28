8b582d7461bf42918a5975e9e637f0158c787437
Kurt Puican <u202016643@upc.edu.pe>
2024-05-28 15:40:09 -0500

    implementacion del profile tutor

--- app/src/main/java/com/example/studymentor/UI/ProfileTutor.kt
+++ app/src/main/java/com/example/studymentor/UI/ProfileTutor.kt
@@ -1,6 +1,5 @@
 package com.example.studymentor.UI
 
-
 import android.content.Intent
 import android.os.Bundle
 import android.widget.ImageButton
@@ -10,12 +9,12 @@ import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.studymentor.R
 
-class TutorProfileActivity : AppCompatActivity() {
+class ProfileTutor: AppCompatActivity(){
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_profile_tutor)
+        setContentView(R.layout.activity_tutor_profile)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
@@ -28,12 +27,12 @@ class TutorProfileActivity : AppCompatActivity() {
         val btTutorList = findViewById<ImageButton>(R.id.btTutors)
 
         btHome.setOnClickListener {
-            val intent = Intent(this@TutorProfileActivity, HomeStudentActivity::class.java)
+            val intent = Intent(this@ProfileTutor, HomeStudentActivity::class.java)
             startActivity(intent)
         }
 
         btTutorList.setOnClickListener {
-            val intent = Intent(this@TutorProfileActivity, TutorListActivity::class.java)
+            val intent = Intent(this@ProfileTutor, TutorListActivity::class.java)
             startActivity(intent)
         }
     }
