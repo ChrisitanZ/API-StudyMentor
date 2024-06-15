ce4664311d570bb62837068f9306d69cffd71d41
RafaelLuyo <u201919295@upc.edu.pe>
2024-06-15 00:12:54 -0500

    feat(student-review): review model and response added

--- app/src/main/java/com/example/studymentor/model/Review.kt
+++ app/src/main/java/com/example/studymentor/model/Review.kt
@@ -1,9 +1,12 @@
 package com.example.studymentor.model
 
+import java.util.Date
+
 data class Review(
     val id: Int,
-    val content: String,
+    val textMessage: String,
     val rating: Int,
     val studentId: Int,
-    val tutorId: Int
+    val tutorId: Int,
+    val date: Date,
 )
